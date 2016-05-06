17e9266fa61c5c73780a8c7f5134e593c372c46e
Fernando <fernando.arpre@gmail.com>
2016-05-05 21:59:03 -0500

    commit fernando buscar punto

---
+++ app/src/main/java/android/dcsdealerperu/com/dealerperu/Activity/ActBuscarPunto.java
@@ -0,0 +1,30 @@
+package android.dcsdealerperu.com.dealerperu.Activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.dcsdealerperu.com.dealerperu.R;
+
+public class ActBuscarPunto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buscar_punto);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
